aa0812e19d42dd168fc2781ec546bec11d663981
thnlsn <52093425+thnlsn@users.noreply.github.com>
2019-08-16 02:20:37 -0700

    added images

---
+++ assets/javascript/game.js
@@ -0,0 +1,35 @@
+var charChosen = false;
+//set to true when first character is clicked
+
+var enemyChosen = false;
+//set to true once a defender is clicked
+//set to false when defender is killed
+
+var
+
+
+
+
+var garruk = {
+    hp: 140,
+    attackPower: 10,
+    counterPower: 32
+}
+
+var sorin = {
+    hp: 80,
+    attackPower: 12,
+    counterPower: 22
+}
+
+var karn = {
+    hp: 230,
+    attackPower: 2,
+    counterPower: 8
+}
+
+var ajani = {
+    hp: 160,
+    attackPower: 6,
+    counterPower: 16
+}
