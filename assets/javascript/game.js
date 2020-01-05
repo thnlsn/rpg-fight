17850ebd97d17b92ab2d8386cfa396da3f0e092a
thnlsn <52093425+thnlsn@users.noreply.github.com>
2020-01-04 21:18:53 -0800

    edited title

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,8 @@
-
 //▓▓▓▓▓▓▓▓▓▓▓▓▓▓ VARIABLES TO BE DETERMINED
 
 var planeswalker;
 var fighter;
 var enemies = [];
-
 
 //▓▓▓▓▓▓▓▓▓▓▓▓▓▓ VARIABLES
 
@@ -26,121 +24,116 @@ var enemy3Alive = true;
 var yourPlaneswalker;
 var yourFighter;
 
-
 //▓▓▓▓▓▓▓▓▓▓▓▓▓▓ PLANESWALKER STATS
 
 var garruk = {
-    name: "garruk",
+    name: 'garruk',
     hp: 140,
     baseAttackPower: 8,
     attackPower: 8,
     counterPower: 28
-}
+};
 
 var sorin = {
-    name: "sorin",
+    name: 'sorin',
     hp: 85,
     baseAttackPower: 16,
     attackPower: 16,
     counterPower: 20
-}
+};
 
 var karn = {
-    name: "karn",
+    name: 'karn',
     hp: 270,
     baseAttackPower: 2,
     attackPower: 2,
     counterPower: 8
-}
+};
 
 var ajani = {
-    name: "ajani",
+    name: 'ajani',
     hp: 215,
     baseAttackPower: 6,
     attackPower: 6,
     counterPower: 12
-}
-
+};
 
 //▓▓▓▓▓▓▓▓▓▓▓▓▓▓ PLANESWALKER CLICKS (make less dry later, try to remove # for variables)
 
-$("#garruk").click(function() {
+$('#garruk').click(function() {
     if (charChosen === false) {
         charChosen = true;
         planeswalker = garruk;
-        $("#garruk").shake(10,5,1);
+        $('#garruk').shake(10, 5, 1);
         console.log(planeswalker);
-        console.log("Character Selected = "+ charChosen);
+        console.log('Character Selected = ' + charChosen);
     } else if (fighterChosen === false && planeswalker !== garruk) {
         fighterChosen = true;
         fighter = garruk;
-        $("#garruk").shake(10,5,1);
+        $('#garruk').shake(10, 5, 1);
         console.log(fighter);
-        console.log("Fighter Selected = "+ charChosen);
+        console.log('Fighter Selected = ' + charChosen);
     }
-    console.log("clicked");
+    console.log('clicked');
 });
 
-$("#sorin").click(function() {
+$('#sorin').click(function() {
     if (charChosen === false) {
         charChosen = true;
         planeswalker = sorin;
-        $("#sorin").shake(10,5,1);
+        $('#sorin').shake(10, 5, 1);
         console.log(planeswalker);
-        console.log("Character Selected = "+ charChosen);
+        console.log('Character Selected = ' + charChosen);
     } else if (fighterChosen === false && planeswalker !== sorin) {
         fighterChosen = true;
         fighter = sorin;
-        $("#sorin").shake(10,5,1);
+        $('#sorin').shake(10, 5, 1);
         console.log(fighter);
-        console.log("Fighter Selected = "+ charChosen);
+        console.log('Fighter Selected = ' + charChosen);
     }
-    console.log("clicked");
-  });
+    console.log('clicked');
+});
 
-$("#karn").click(function() {
+$('#karn').click(function() {
     if (charChosen === false) {
         charChosen = true;
         planeswalker = karn;
-        $("#karn").shake(10,5,1);
+        $('#karn').shake(10, 5, 1);
         console.log(planeswalker);
-        console.log("Character Selected = "+ charChosen);
+        console.log('Character Selected = ' + charChosen);
     } else if (fighterChosen === false && planeswalker !== karn) {
         fighterChosen = true;
         fighter = karn;
-        $("#karn").shake(10,5,1);
+        $('#karn').shake(10, 5, 1);
         console.log(fighter);
-        console.log("Fighter Selected = "+ charChosen);
+        console.log('Fighter Selected = ' + charChosen);
     }
-    console.log("clicked");
+    console.log('clicked');
 });
 
-$("#ajani").click(function() {
+$('#ajani').click(function() {
     if (charChosen === false) {
         charChosen = true;
         planeswalker = ajani;
-        $("#ajani").shake(10,5,1);
+        $('#ajani').shake(10, 5, 1);
         console.log(planeswalker);
-        console.log("Character Selected = "+ charChosen);
+        console.log('Character Selected = ' + charChosen);
     } else if (fighterChosen === false && planeswalker !== ajani) {
         fighterChosen = true;
         fighter = ajani;
-        $("#ajani").shake(10,5,1);
+        $('#ajani').shake(10, 5, 1);
         console.log(fighter);
-        console.log("Fighter Selected = "+ charChosen);
+        console.log('Fighter Selected = ' + charChosen);
     }
-    console.log("clicked");
-
+    console.log('clicked');
 });
-
-
 
 //▓▓▓▓▓▓▓▓▓▓▓▓▓▓ MOVEMENT FUNCTIONS
 
 function initialMove() {
-    $("garruk").detach();
-    garruk.appendTo("player-container");
-    
+    $('garruk').detach();
+    garruk.appendTo('player-container');
+
     //move chosen(ex. planeswalker) to player-container
     //move others to enemy-container
 }
@@ -149,132 +142,79 @@ function initialMove() {
     //move enemyChosen to fighter-container
 } */
 
-
-
-
 //▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ATTACK CLICK
 
-$("#attack-button").click(function() {
+$('#attack-button').click(function() {
     if (charChosen === true && fighterChosen === true) {
         if (fighter.hp >= 0 && planeswalker.hp >= 0) {
             combat(planeswalker, fighter);
-            update("#" + planeswalker.name + "-hp", planeswalker.hp);
-            update("#" + fighter.name + "-hp", fighter.hp);
+            update('#' + planeswalker.name + '-hp', planeswalker.hp);
+            update('#' + fighter.name + '-hp', fighter.hp);
         } else if (fighter.hp <= 0) {
             //remove fighter from game and html
             fighter;
             fighterChosen = false;
             if (enemy1Alive === true) {
                 enemy1Alive = false;
-                console.log("1:" + enemy1Alive + " 2:" +  enemy2Alive + " 3:" + enemy3Alive);
+                console.log(
+                    '1:' +
+                        enemy1Alive +
+                        ' 2:' +
+                        enemy2Alive +
+                        ' 3:' +
+                        enemy3Alive
+                );
             } else if (enemy2Alive === true) {
                 enemy2Alive = false;
-                console.log("1:" + enemy1Alive + " 2:" +  enemy2Alive + " 3:" + enemy3Alive);
+                console.log(
+                    '1:' +
+                        enemy1Alive +
+                        ' 2:' +
+                        enemy2Alive +
+                        ' 3:' +
+                        enemy3Alive
+                );
             } else {
                 enemy3Alive === false;
-                console.log("1:" + enemy1Alive + " 2:" +  enemy2Alive + " 3:" + enemy3Alive);
-                alert("Contratulations! The multiverse is saved!")
+                console.log(
+                    '1:' +
+                        enemy1Alive +
+                        ' 2:' +
+                        enemy2Alive +
+                        ' 3:' +
+                        enemy3Alive
+                );
+                alert('Contratulations! The multiverse is saved!');
             }
         } else {
-            alert("Sorry, you've lost! Refresh to try again.")
+            alert("Sorry, you've lost! Refresh to try again.");
         }
     }
-    console.log("clicked");
+    console.log('clicked');
 });
-
 
 //▓▓▓▓▓▓▓▓▓▓▓▓▓▓ COMBAT CALCULATION FUNCTIONS
 
 function combat(player, computer) {
     if (firstAttack === true) {
-    firstAttack = false;
-    computer.hp = computer.hp - player.attackPower;
-    console.log(computer.hp);
-    player.hp = player.hp - computer.counterPower;
-    console.log(player.hp);
+        firstAttack = false;
+        computer.hp = computer.hp - player.attackPower;
+        console.log(computer.hp);
+        player.hp = player.hp - computer.counterPower;
+        console.log(player.hp);
     } else {
-    player.attackPower = player.attackPower + player.baseAttackPower;
-    computer.hp = computer.hp - player.attackPower;
-    console.log(computer.hp);
-    player.hp = player.hp - computer.counterPower;
-    console.log(player.hp);
+        player.attackPower = player.attackPower + player.baseAttackPower;
+        computer.hp = computer.hp - player.attackPower;
+        console.log(computer.hp);
+        player.hp = player.hp - computer.counterPower;
+        console.log(player.hp);
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
 function update(id, value) {
     //  document.getElementById(id).innerHTML = value;
-        $(id).html(value);
-    };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    $(id).html(value);
+}
 
 /*
 
@@ -290,18 +230,18 @@ $(document).ready(function () {
 
 */
 
-
-
-
 //▓▓▓▓▓▓▓▓▓▓▓▓▓▓ FUNCTION TO SHAKE ONCLICK
-jQuery.fn.shake = function(interval,distance,times){
-    interval = typeof interval == "undefined" ? 100 : interval;
-    distance = typeof distance == "undefined" ? 10 : distance;
-    times = typeof times == "undefined" ? 3 : times;
+jQuery.fn.shake = function(interval, distance, times) {
+    interval = typeof interval == 'undefined' ? 100 : interval;
+    distance = typeof distance == 'undefined' ? 10 : distance;
+    times = typeof times == 'undefined' ? 3 : times;
     var jTarget = $(this);
-    jTarget.css('position','relative');
-    for(var iter=0;iter<(times+1);iter++){
-       jTarget.animate({ left: ((iter%2==0 ? distance : distance*-1))}, interval);
+    jTarget.css('position', 'relative');
+    for (var iter = 0; iter < times + 1; iter++) {
+        jTarget.animate(
+            { left: iter % 2 == 0 ? distance : distance * -1 },
+            interval
+        );
     }
-    return jTarget.animate({ left: 0},interval);
-}
+    return jTarget.animate({ left: 0 }, interval);
+};
